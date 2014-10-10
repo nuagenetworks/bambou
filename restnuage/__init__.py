2bdcd82046304eabe4815586cc39bfd1900ab369
Christophe Serafin <christophe.serafin@gmail.com>
2014-10-10 16:51:39 -0700

    removes config

--- restnuage/__init__.py
+++ restnuage/__init__.py
@@ -3,10 +3,6 @@
 import logging
 logging.getLogger('restnuage').addHandler(logging.NullHandler())
 
-from ConfigParser import ConfigParser
-config = ConfigParser()
-config.read('./settings.cfg')
-
 __all__ = ['NURESTBasicUser', 'NURESTConnection', 'NURESTFetcher', 'NURESTLoginController', 'NURESTObject', 'NURESTPushCenter', 'NURESTRequest', 'NURESTResponse']
 
 from restnuage.nurest_user import NURESTBasicUser
