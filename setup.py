51282775617052f13504faaef057ba784051ce3a
Antoine Mercadal <antoine.mercadal@gmail.com>
2015-03-27 12:15:39 -0700

    embed contextual

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     url='http://www.nuagenetworks.net/',
     author='Christophe Serafin',
     author_email='christophe.serafin@alcatel-lucent.com',
-    packages=['bambou', 'bambou.utils'],
+    packages=['bambou', 'bambou.utils', 'bambou.contextual'],
     description='REST Library for Nuage Networks',
     long_description=open('README.md').read(),
     install_requires=[line for line in open('requirements.txt')],
