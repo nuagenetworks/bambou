ebd4dd6940c1263f3945d3dcb6a2f7e6641c9567
Antoine Mercadal <antoine.mercadal@gmail.com>
2015-03-27 12:33:14 -0700

    update the setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,6 @@ from setuptools  import setup
 setup(
     name='bambou',
     version='0.0.1',
-    url='http://www.nuagenetworks.net/',
     author='Christophe Serafin',
     author_email='christophe.serafin@nuagenetworks.net',
     packages=['bambou', 'bambou.utils', 'bambou.contextual'],
