8a73ba5f7b7ce6e27c2a630bb3f4d20ff6d44aed
Antoine Mercadal <antoine.mercadal@gmail.com>
2015-05-19 10:46:41 -0700

    remove the warning fix again...

--- bambou/__init__.py
+++ bambou/__init__.py
@@ -12,9 +12,6 @@ from __future__ import print_function
 
 import logging
 
-import requests
-requests.packages.urllib3.disable_warnings()
-
 bambou_logger = logging.getLogger('bambou')
 
 try:  # Python 2.7+
