a56d5a288ddf00a0482f6e00446d16fa693043a9
Christophe Serafin <christophe.serafin@gmail.com>
2015-08-17 11:21:41 -0700

    Updated License to BSD-3 and prepared github deployment

--- setup.py
+++ setup.py
@@ -1,18 +1,45 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2015, Alcatel-Lucent Inc
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#     * Redistributions of source code must retain the above copyright
+#       notice, this list of conditions and the following disclaimer.
+#     * Redistributions in binary form must reproduce the above copyright
+#       notice, this list of conditions and the following disclaimer in the
+#       documentation and/or other materials provided with the distribution.
+#     * Neither the name of the copyright holder nor the names of its contributors
+#       may be used to endorse or promote products derived from this software without
+#       specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY
+# DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
 import os
 from setuptools  import setup
 
-version = os.environ.get("BAMBOU_VERSION", "0.0.1")
+version = os.environ.get("BAMBOU_VERSION", "1.0.0")
 
 setup(
     name='bambou',
     version=version,
-    author='Christophe Serafin',
-    author_email='christophe.serafin@nuagenetworks.net',
+    author='Christophe Serafin, Antoine Mercadal',
+    author_email='christophe.serafin@nuagenetworks.net, antoine@nuagenetworks.net',
     packages=['bambou', 'bambou.utils', 'bambou.contextual'],
     description='REST Library for Nuage Networks',
     long_description=open('README.md').read(),
     install_requires=[line for line in open('requirements.txt')],
-    license='TODO',
+    license='BSD-3',
     url='https://github.com/nuagenetworks',
     classifiers=[
         "Development Status :: 3 - Alpha",
