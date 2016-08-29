c64bfb14019fb34bb2114c685d28e896c0ed386b
Corentin Henry <corentinhenry@gmail.com>
2016-08-29 09:13:55 -0700

    fix headers to comply with requests>=2.11
    as of 2.11, requests raises an exception if header values are not `str`
    or `bytes`.
    
    see: https://github.com/kennethreitz/requests/issues/3477

--- bambou/nurest_request.py
+++ bambou/nurest_request.py
@@ -44,7 +44,7 @@ class NURESTRequest(object):
             self.set_header('X-Nuage-Filter', filter)
 
         if page:
-            self.set_header('X-Nuage-Page', page)
+            self.set_header('X-Nuage-Page', str(page))
 
         if order_by:
             self.set_header('X-Nuage-OrderBy', order_by)
@@ -117,5 +117,9 @@ class NURESTRequest(object):
 
     def set_header(self, header, value):
         """ Set header value """
-
+        # requests>=2.11 only accepts `str` or `bytes` header values
+        # raising an exception here, instead of leaving it to `requests` makes
+        # it easy to know where we passed a wrong header type in the code.
+        if not isinstance(value, (str, bytes)):
+            raise TypeError("header values must be str or bytes, but %s value has type %s" % (header, type(value)))
         self._headers[header] = value
