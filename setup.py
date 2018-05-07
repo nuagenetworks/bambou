d334fea23e384d3df8e552fe1849ad707941c666
Philippe Dellaert <philippe@dellaert.org>
2018-05-06 21:24:33 -0700

    Updating version to 3.0.1 after validation improvements

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ from setuptools import setup
 
 setup(
     name='bambou',
-    version='3.0.0',
+    version='3.0.1',
     author='Nuage Networks',
     author_email='opensource@nuagenetworks.net',
     packages=['bambou', 'bambou.utils'],
